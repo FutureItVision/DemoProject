747cdf3800c35141046e879e68332f90e3d5e94d
nadiruzzamanmuhammad <nadiruzzamanqa@gamil.com>
2019-02-26 20:42:29 -0500

    Updated

--- src/BasicTest/TestOne.java
+++ src/BasicTest/TestOne.java
@@ -6,23 +6,23 @@ import org.testng.annotations.Test;
 
 public class TestOne {
 
-    public WebDriver driver;
+   // public RemoteWebDriver driver;
 	@Test
 	public void firstTest(){
 
-    ChromeOptions options = new ChromeOptions();
-                        options.addArguments("--disable-dev-shm-usage");
-                        options.addArguments("--no-sandbox");
-                        options.setAcceptInsecureCerts(true);
-                        options.setHeadless(true);
-
-    System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
-    ChromeDriver driver = new ChromeDriver(options);
-    driver.get("http://automationpractice.com/index.php");
-    driver.findElementByXPath(".//*[@id='header_logo']/a/img").isDisplayed();
-//        driver = new RemoteWebDriver(new URL("http://172.217.11.46:8000/wd/hub"), chromeCapabilities);
-//        driver.get("http://automationpractice.com/index.php");
-//        driver.findElementByXPath(".//*[@id='header_logo']/a/img").isDisplayed();
+//    ChromeOptions options = new ChromeOptions();
+//                        options.addArguments("--disable-dev-shm-usage");
+//                        options.addArguments("--no-sandbox");
+//                        options.setAcceptInsecureCerts(true);
+//                        options.setHeadless(true);
+//
+//    System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+//    ChromeDriver driver = new ChromeDriver(options);
+//    driver.get("http://automationpractice.com/index.php");
+//    driver.findElementByXPath(".//*[@id='header_logo']/a/img").isDisplayed();
+        driver = new RemoteWebDriver(new URL("http://172.217.11.46:8000/wd/hub"), chromeCapabilities);
+        driver.get("http://automationpractice.com/index.php");
+        driver.findElementByXPath(".//*[@id='header_logo']/a/img").isDisplayed();
     driver.close();
 				
 	}
