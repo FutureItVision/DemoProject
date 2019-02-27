a90fb2c044dc9b0df0e5b5d83d9051446eada757
nadiruzzamanmuhammad <nadiruzzamanqa@gamil.com>
2019-02-26 19:22:36 -0500

    Updated

--- src/BasicTest/TestOne.java
+++ src/BasicTest/TestOne.java
@@ -10,19 +10,19 @@ public class TestOne {
 	@Test
 	public void firstTest(){
 
-//    ChromeOptions options = new ChromeOptions();
-//                        options.addArguments("--disable-dev-shm-usage");
-//                        options.addArguments("--no-sandbox");
-//                        options.setAcceptInsecureCerts(true);
-//                        options.setHeadless(true);
-//
-//    System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
-//    ChromeDriver driver = new ChromeDriver(options);
-//    driver.get("http://automationpractice.com/index.php");
-//    driver.findElementByXPath(".//*[@id='header_logo']/a/img").isDisplayed();
-        driver = new RemoteWebDriver(new URL("http://localhost:80/wd/hub"), chromeCapabilities);
-        driver.get("http://automationpractice.com/index.php");
-        driver.findElementByXPath(".//*[@id='header_logo']/a/img").isDisplayed();
+    ChromeOptions options = new ChromeOptions();
+                        options.addArguments("--disable-dev-shm-usage");
+                        options.addArguments("--no-sandbox");
+                        options.setAcceptInsecureCerts(true);
+                        options.setHeadless(true);
+
+    System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+    ChromeDriver driver = new ChromeDriver(options);
+    driver.get("http://automationpractice.com/index.php");
+    driver.findElementByXPath(".//*[@id='header_logo']/a/img").isDisplayed();
+//        driver = new RemoteWebDriver(new URL("http://localhost:80/wd/hub"), chromeCapabilities);
+//        driver.get("http://automationpractice.com/index.php");
+//        driver.findElementByXPath(".//*[@id='header_logo']/a/img").isDisplayed();
     driver.close();
 				
 	}
